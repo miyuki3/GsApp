7732955022e8743e913d7509864f0735d1a89277
miyuki <migitaso@gmail.com>
2017-01-14 16:19:59 +0900

    お天気APIを読み込んだ

---
+++ GsApp/app/src/main/java/com/gashfara/miyuki/gsapp/MessageRecord.java
@@ -0,0 +1,22 @@
+//1つのセルにあるデータを保存するためのデータクラスです。
+package com.gashfara.miyuki.gsapp;
+
+public class MessageRecord {
+    //保存するデータ全てを変数で定義します。privateにしておくと変数名をあとから変えられる。公開したい部分と隠したい部分を制御できるのがJava。
+    //newて呼び出さないと、classは作られない。
+    private String imageUrl;
+    private String comment;
+
+    //データを１つ作成する関数です。項目が増えたら増やしましょう。MessageRecordはコンストラクターという.
+    public MessageRecord(String imageUrl, String comment) {
+        this.imageUrl = imageUrl;
+        this.comment = comment;
+    }
+    //それぞれの項目を返す関数です。項目が増えたら増やしましょう。getするためのfuncion。
+    public String getComment() {
+        return comment;
+    }
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
